330a44f12aae05f5f4cf59fab94b2660a080ab43
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-04-06 15:52:16 -0500

    first commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Isaac Urbina on 4/6/25.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
