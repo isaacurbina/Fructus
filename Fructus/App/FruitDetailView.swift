72f6a132cbc6a8dc93a9c149c3aceadae2df4b16
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-04-11 19:44:59 -0500

    88. 6. Learn how to create a detail view layout in SwiftUI - Part 1 (#6)

---
+++ Fructus/App/FruitDetailView.swift
@@ -0,0 +1,75 @@
+//
+//  FruitDetailView.swift
+//  Fructus
+//
+//  Created by Isaac Urbina on 4/11/25.
+//
+
+import SwiftUI
+
+struct FruitDetailView: View {
+	
+	
+	// MARK: - properties
+	
+	var fruit: Fruit
+	
+	
+	// MARK: - body
+	
+    var body: some View {
+		NavigationView {
+			ScrollView(.vertical, showsIndicators: false) {
+				VStack(alignment: .center, spacing: 20) {
+					
+					// header
+					FruitHeaderView(fruit: fruit)
+					
+					VStack(alignment: .leading, spacing: 20) {
+						// title
+						Text(fruit.title)
+							.font(.largeTitle)
+							.fontWeight(.heavy)
+							.foregroundColor(fruit.gradientColors[1])
+						
+						// headline
+						Text(fruit.headline)
+							.font(.headline)
+							.multilineTextAlignment(.leading)
+						
+						// nutrients
+						
+						
+						// subheadline
+						Text("Learn more about \(fruit.title)".uppercased())
+							.fontWeight(.bold)
+							.foregroundColor(fruit.gradientColors[1])
+						
+						// description
+						Text(fruit.description)
+							.multilineTextAlignment(.leading)
+						
+						// link
+						SourceLinkView()
+							.padding(.top, 10)
+							.padding(.bottom, 40)
+						
+					} // VStack
+					.padding(.horizontal, 20)
+					.frame(maxWidth: 640, alignment: .center)
+					
+				} // VStack
+				.navigationBarTitle(fruit.title, displayMode: .inline)
+				.navigationBarHidden(true)
+				
+			} // ScrollView
+			.edgesIgnoringSafeArea(.top)
+			
+		} // NavigationView
+    }
+}
+
+#Preview {
+	FruitDetailView(fruit: fruitsData[0])
+		.previewLayout(.sizeThatFits)
+}
---
+++ Fructus/View/FruitHeaderView.swift
@@ -0,0 +1,46 @@
+//
+//  FruitHeaderView.swift
+//  Fructus
+//
+//  Created by Isaac Urbina on 4/11/25.
+//
+
+import SwiftUI
+
+struct FruitHeaderView: View {
+	
+	// MARK: - properties
+	
+	var fruit: Fruit
+	@State private var isAnimatingImage: Bool = false
+	
+	
+	// MARK: - body
+	
+    var body: some View {
+		ZStack {
+			LinearGradient(gradient: Gradient(colors: fruit.gradientColors), startPoint: .topLeading, endPoint: .bottomTrailing)
+			
+			Image(fruit.image)
+				.resizable()
+				.scaledToFit()
+				.shadow(
+					color: Color(red: 0, green: 0, blue: 0, opacity: 0.15),
+					radius: 9, x: 6, y: 8
+				)
+				.padding(.vertical, 20)
+				.scaleEffect(isAnimatingImage ? 1.0 : 0.6)
+		} // ZStack
+		.frame(height: 440)
+		.onAppear() {
+			withAnimation(.easeOut(duration: 0.5)) {
+				isAnimatingImage = true
+			}
+		}
+    }
+}
+
+#Preview {
+	FruitHeaderView(fruit: fruitsData[0])
+		.previewLayout(.fixed(width: 375, height: 440))
+}
