56d5d1f66593c0545e83423b3d12a3c2a9fc2892
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-04-13 18:32:39 -0500

    91. 9. Let's create an outstanding Settings View - Part 2 (#9)

--- Fructus/App/SettingsView.swift
+++ Fructus/App/SettingsView.swift
@@ -23,6 +23,7 @@ struct SettingsView: View {
 				VStack(spacing: 20) {
 					
 					// mark - section 1
+					
 					GroupBox(label: SettingsLabelView(labelText: "Fructus", labelImage: "info.circle")) {
 						Divider().padding(.vertical, 4)
 						HStack(alignment: .center, spacing: 10) {
@@ -40,6 +41,18 @@ struct SettingsView: View {
 					
 					
 					// mark - section 3
+					
+					GroupBox(
+						label: SettingsLabelView(labelText: "Application", labelImage: "apps.iphone")
+					) {						
+						SettingsRowView(name: "Developer", content: "John / Jane")
+						SettingsRowView(name: "Designer", content: "Robert Petras")
+						SettingsRowView(name: "Compatibility", content: "iOS 14")
+						SettingsRowView(name: "Website", linkLabel: "SwiftUI Masterclass", linkDestination: "swiftuimasterclass.com")
+						SettingsRowView(name: "Twitter", linkLabel: "@RobertPetras", linkDestination: "twitter.com/robertpetras")
+						SettingsRowView(name: "SwiftUI", content: "2.0")
+						SettingsRowView(name: "Version", content: "1.1.0")
+					}
 					
 					
 				} // VStack
